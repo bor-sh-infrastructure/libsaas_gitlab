f2faf1f902b70c1987edf4cb8f6cd7bb4ad1f37b
b-sh <b-sh@gmx.net>
2015-05-15 19:55:55 +0200

    Update setup version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import os
 from distutils.core import setup, Command
 from setuptools import setup, find_packages
 
-VERSION = (0, 1, 0, 'dev')
+VERSION = (0, 2, 0, 'dev')
 
 __version__   = ".".join(map(str, VERSION[0:3])) + "".join(VERSION[3:])
 __author__    = "b-sh"
